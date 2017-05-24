b05ce018db61f4c501acc07f1d7ef0a48e73c0a9
colmconn <colmconn+github@gmail.com>
2017-05-24 12:58:13 -0700

    Sort the dcm_info.txt file based on series number (the first column in said file).

--- dcm_check.sh
+++ dcm_check.sh
@@ -9,6 +9,15 @@ rm -f dcm_info.txt
 find . -type d \! -name . -prune | while read d; do
     f=$( ls $d/*.DCM | head -1 )
     description=$( dicom_hdr ${f} | grep "Desc" | head -n1 | awk -F"//" '{print $3}' )
-    echo $d $f $description
-    echo $d $f $description >> dcm_info.txt    
+
+    ## the $d is actually the series order, so 1 indicates first
+    ## scan/series, 2, the second , and so on.  Those with 3 digits
+    ## were the result of post processing on the scanner console and
+    ## to all intents and purposes are of no interest to us
+
+    ## echo $( basename $d ) $f $description 
+    echo $( basename $d ) $f $description >> dcm_info.txt    
 done
+
+## sort the dcm_info by series order, i.e., the first field
+cat dcm_info.txt | sort -n -k1 > dcm_info.txt.new && mv -f dcm_info.txt.new dcm_info.txt
